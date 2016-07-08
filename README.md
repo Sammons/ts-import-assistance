90339075aed935ae244e5e343162172a4cc7bd3d
Sammons <ben@hyperfish.com>
2016-07-08 02:17:10 -0700

    refactor, update readme, handle typings and node modules better, and do a slightly smarter guess

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# 0.0.4
+
+* do not consider "<" or ">" part of symbols when guessing symbols to import.
+* stop ignoring periods; fs.readFileSync (cursor here) will now try to look up "readFileSync" first instead of "fsreadFileSync"
+* for core node modules do "import * as module from 'module'" when possible, e.g. fs
+* for modules in typings do "import * as module from 'module'" when possible, e.g. using bluebird
+* stop clobbering "use strict"
+* updates to readme / packaging
+
 # 0.0.3
 
 * instead of importing `../node_modules/.../module_name/file`, import `module_name/file`
@@ -9,17 +18,26 @@ packaging updates
 
 # 0.0.1 Snapshot release
 
-TypeScript assistance with writing imports.
+TypeScript assistance with writing imports. Written with node.js development in mind.
 
-cmd + shift + p, then "Resolve and import symbol".
+cmd + shift + p, then type "Resolve and import symbol". This normally autocompletes just typing "res".
 
-The extension command is `resolveMissingImportsForHighlighted`
+The extension command is `extension.resolveMissingImportsForHighlighted`, if you want to bind it to a hotkey, for example you could use the following in keybindings:
+
+
+    {
+        "key": "cmd+i", "command": "extension.resolveMissingImportsForHighlighted"
+    }
+
 
 This will search for symbols matching whatever you have highlighted, and then import them into the current file.
-If nothing is highlighted, it makes a best guess using text before the cursor. 
+
+If nothing is highlighted, it makes a best guess using text before the cursor.
+
 This tool isn't very smart right now, it will blindly attempt to import things even if they exist in scope.
 
 
 Cheers!
+Source [here](https://github.com/Sammons/ts-import-assistance)
 
 This software is utterly free and open, and the authors are not responsible for any consequences of its existence (MIT license).
--- src/extension.ts
+++ src/extension.ts
@@ -1,97 +1,210 @@
 'use strict';
 
-// for typescript, hacky solution for auto importing on demand.
-
+// for typescript, simple solution for auto importing on demand.
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as os from 'os';
 
-var nodeLibs =
+const nodeLibs =
     ['path', 'events', 'child_process', 'fs', 'os', 'net', 'crypto',
         'util', 'stream', 'readline', 'tty', 'repl', 'punycode', 'querystring',
         'string_decoder', 'http', 'https', 'cluster', 'assert', 'dgram', 'url', 'vm', 'zlib', 'dns'];
+const newLine = '\n';//TODO: would it actually be better to be platform specific? I'm not convinced.
+const tsImportAssistanceCommandName = 'extension.resolveMissingImportsForHighlighted';
 
+type ImportDetails = { symbolToImport: string; path: string; symbolInfo: vscode.SymbolInformation };
 
-export function activate(context: vscode.ExtensionContext) {
+class TSImportAssistance {
+    activeFile = vscode.window.activeTextEditor.document.fileName;
+    activeDir = path.parse(this.activeFile).dir;
+    insertPosition = new vscode.Position(0, 0);
+    _textBeforeOrInCursor: string = null;
+    _textFragmentsInSelectionOrBeforeCursor: string[] = null;
 
-    console.log('Activated');
-    let disposable = vscode.commands.registerCommand('extension.resolveMissingImportsForHighlighted', () => {
-        var selection = vscode.window.activeTextEditor.selection;
-        var activeFile = vscode.window.activeTextEditor.document.fileName;
-        var activeDir = path.parse(activeFile).dir;
-        var documentText = vscode.window.activeTextEditor.document.getText().replace(/\r\n/gm, '\n');
-        var importLines = documentText.split(/\n/gm).filter(l => /^import/.test(l.trim()));
-        var imports = importLines.map(i => i.split('from').pop().replace(/[\';\"]*/gm, '').trim());
+    moveImportInsertsToSecondLineIfUseStrictEnabled(): void {
+        let selection = vscode.window.activeTextEditor.selection;
 
-        var addToImports = (symbol, filePath) => {
-            let performEdit = () => {
-                return vscode.window.activeTextEditor.edit((editbuilder) => {
-                    var resolvedPosixPath = path.normalize(filePath).replace(/\\/gm, '/');
-                    var cleanPath = resolvedPosixPath.replace(/(\.js)|(\.d.ts)|(\.ts)$/gm, '');
-                    if (cleanPath != filePath && filePath[0] != '.') {
-                        cleanPath = './' + cleanPath;
-                    }
-                    cleanPath = cleanPath.split('node_modules/').pop();
-                    editbuilder.insert(new vscode.Position(0, 0), `import {${symbol}} from '${cleanPath}';\n`);
-                });
-            };
-            return performEdit();
-        };
-
-        console.log('imports', imports);
         if (selection.isEmpty) {
-            var range = new vscode.Range(new vscode.Position(0, 0), selection.start);
-            var selectionText = vscode.window.activeTextEditor.document.getText(range);
-            var symbols = selectionText.split(/\s/gm)
-            // TODO: super finicky with wierd symbols, but gets the job done for me.
-            var nextSymbol = () => symbols.pop().replace(/[\{\}\(\)\`\'\"\[\]\;\.\*\-\%\@\~\/]*/gm, '');
-            var lastSymbol = nextSymbol();
-            // TODO: maybe iterate and reject symbols till we get one that could work?
-            var selectionText = lastSymbol;
+            let range = new vscode.Range(new vscode.Position(0, 0), selection.start);
+            this._textBeforeOrInCursor = vscode.window.activeTextEditor.document.getText(range);
         } else {
-            var selectionText = vscode.window.activeTextEditor.document.getText(selection);
+            this._textBeforeOrInCursor = vscode.window.activeTextEditor.document.getText(selection);
         }
-        console.log('cur symbol text', selectionText)
-        Promise.resolve(vscode.commands.executeCommand('vscode.executeWorkspaceSymbolProvider', selectionText).then(
-            (result: vscode.SymbolInformation[]) => {
-                result = result.filter(
-                    r => r.location.uri.fsPath !== activeFile
-                        && r.name === selectionText
-                        && r.kind !== vscode.SymbolKind.Property);
-                if (result.length === 0) {
-                    vscode.window.showInformationMessage(`Unable to locate symbol ${selectionText}`);
-                } else if (result.length > 1) {
-                    console.log('possibilities', result);
-                    var paths = {};
-                    result.map(r => r.location.uri.fsPath).forEach(p => paths[p] = true);
-                    var options = Object.keys(paths);
-                    return vscode.window.showQuickPick(options).then(pick => {
-                        console.log('picked', pick)
-                        var pickedChoices = result.filter(r => r.location.uri.fsPath === pick);
-                        var choicesMatchingNodeLibs = pickedChoices.filter(r => !!nodeLibs.find(l => l === r.containerName));
-                        console.log('cur path', vscode.window.activeTextEditor.document.fileName)
-                        if (choicesMatchingNodeLibs.length > 0) {
-                            console.log('picking node lib', choicesMatchingNodeLibs[0].containerName);
-                            return addToImports(selectionText, choicesMatchingNodeLibs[0].containerName);
-                        } else {
-                            // whatever, pick the first one
-                            console.log('picking the first one');
-                            return addToImports(selectionText, path.relative(activeDir, pickedChoices[0].location.uri.fsPath));
-                        }
-                    });
-                } else {
-                    // console.log('result', result[0]);
-                    // console.log('resolved path', path.relative(activeDir, result[0].location.uri.fsPath));
-                    // console.log('cur path', vscode.window.activeTextEditor.document.fileName)
-                    return addToImports(selectionText, path.relative(activeDir, result[0].location.uri.fsPath));
+        // TODO: super finicky with wierd symbols, but gets the job done for me.
+        this._textFragmentsInSelectionOrBeforeCursor =
+            this._textBeforeOrInCursor.split(/[\s\{\}\(\)\`\'\"\[\]\;\*\-\%\@\~\/\<\>\.]/gm)
+                .filter(s => s.trim() !== '');
+
+        let startOfFile =
+            vscode.window.activeTextEditor.document.getText(new vscode.Range(
+                new vscode.Position(0, 0),
+                new vscode.Position(0, "'use strict'".length)
+            ));
+        if (startOfFile.replace(/\"/g, '\'') === "'use strict'") {
+            this.insertPosition = new vscode.Position(1, 0);
+        }
+    }
+
+    constructor() {
+        this.moveImportInsertsToSecondLineIfUseStrictEnabled();
+    }
+
+    cleanPathToImportFrom(pathStr: string): string {
+        let cleanPath = pathStr;
+        if (!nodeLibs.find(s => s === pathStr)) {
+            let resolvedPosixPath = path.normalize(pathStr).replace(/\\/gm, '/');
+            resolvedPosixPath = path.relative(this.activeDir, resolvedPosixPath);
+            cleanPath = resolvedPosixPath.replace(/(\.js)|(\.d.ts)|(\.ts)$/gm, '');
+        }
+        if (cleanPath != pathStr && pathStr[0] != '.') {
+            cleanPath = './' + cleanPath;
+        }
+        if (cleanPath.includes('node_modules')) {
+            cleanPath = cleanPath.split('node_modules/').pop();
+        }
+        if (cleanPath.includes('typings')) {
+            // typings is usually structured like this:
+            // typings/browser/definitions/whatever/index.d.ts
+            // typings/main/definitions/whatever/index.d.ts
+            cleanPath = cleanPath.split('definitions/').pop().split('/')[0];
+        }
+        return cleanPath;
+    }
+
+    constructImportStatement(details: ImportDetails): string {
+        let cleanPath = this.cleanPathToImportFrom(details.path);
+        if (nodeLibs.find(s => s === cleanPath) || cleanPath === details.symbolToImport) {
+            return `import * as ${details.symbolToImport} from '${cleanPath}';${newLine}`;
+        } else {
+            return `import {${details.symbolToImport}} from '${cleanPath}';${newLine}`;
+        }
+    }
+
+    addToImports(details: ImportDetails): PromiseLike<boolean> {
+        return vscode.window.activeTextEditor.edit((editbuilder) => {
+            let importStatement = this.constructImportStatement(details);
+            editbuilder.insert(this.insertPosition, importStatement);
+        });
+    };
+
+    getCurrentImports() {
+        let documentText = vscode.window.activeTextEditor.document.getText().replace(/\r\n/gm, '\n');
+        let importLines = documentText.split(/\n/gm).filter(l => /^import/.test(l.trim()));
+        let imports = importLines.map(i => i.split('from').pop().replace(/[\';\"]*/gm, '').trim());
+        return imports;
+    }
+
+    getCoreModuleUsage(str: string) {
+        let pieces = str.split('.');
+        let coreModule = nodeLibs.find(n => n === pieces[0]);
+        return coreModule;
+    };
+
+    guessSymbolToImport(): string {
+        // TODO: maybe iterate and reject symbols till we get one that could work? usually doesn't matter
+        let selectionText = this._textFragmentsInSelectionOrBeforeCursor[
+            this._textFragmentsInSelectionOrBeforeCursor.length - 1
+        ];
+        console.log('cur symbol text', selectionText);
+        let modul = this.getCoreModuleUsage(selectionText);
+        if (modul) {
+            return modul;
+        }
+        return selectionText;
+    }
+
+    runBuiltinSymbolSearch(symbolToTryToImport: string): PromiseLike<vscode.SymbolInformation[]> {
+        return vscode.commands.executeCommand('vscode.executeWorkspaceSymbolProvider', symbolToTryToImport);
+    }
+
+    filterSymbolInformationToRelevant(symbolInfo: vscode.SymbolInformation[], desiredSymbolStr: string) {
+        return symbolInfo.filter(
+            r => r.location.uri.fsPath !== this.activeFile // importing from current file isn't useful
+                && r.name === desiredSymbolStr // must match
+        );
+    }
+
+
+    getSetOfCleanFsPathsFromSymbolInfos(symbolInfos: vscode.SymbolInformation[]): Array<string> {
+        let paths = {};
+        symbolInfos.map(r => r.location.uri.fsPath).forEach(p => paths[this.cleanPathToImportFrom(p)] = true);
+        return Object.keys(paths);
+    }
+
+    pickSymbolToUseInImport(symbolInfos: vscode.SymbolInformation[], desiredSymbolStr: string): PromiseLike<ImportDetails> {
+        console.log('possibilities', symbolInfos);
+        let paths = this.getSetOfCleanFsPathsFromSymbolInfos(symbolInfos);
+        return vscode.window.showQuickPick(paths, {
+            placeHolder: 'Choose which location you intended to import from.'
+        }).then(userPick => {
+            console.log('picked', userPick);
+            if (!userPick) {
+                return null;
+            }
+            let symbolInfosMatchingUserChoice = symbolInfos.filter(
+                r => this.cleanPathToImportFrom(r.location.uri.fsPath) === userPick);
+
+            let choicesMatchingNodeLibs = symbolInfosMatchingUserChoice.filter(
+                r => !!nodeLibs.find(l => l === r.containerName));
+
+            if (choicesMatchingNodeLibs.length > 0) {
+                let choice = choicesMatchingNodeLibs[0];
+                console.log('picking node lib', choice.containerName);
+                return { symbolToImport: desiredSymbolStr, path: choice.containerName, symbolInfo: choice };
+            } else {
+                // TODO: consider, what if symbolInfosMatchingUserChoice.length > 1 ?
+                let choice = symbolInfosMatchingUserChoice[0];
+                console.log('picking the first selection');
+                return { symbolToImport: desiredSymbolStr, path: choice.location.uri.fsPath, symbolInfo: choice };
+            }
+        });
+    }
+
+    // can return null
+    searchForSymbolInWorkspace(symbolToTryToImport: string): PromiseLike<ImportDetails> | ImportDetails {
+        if (nodeLibs.find(s => s === symbolToTryToImport)) {
+            return { // just short cut to return node module such as 'fs'
+                symbolInfo: null,
+                path: symbolToTryToImport,
+                symbolToImport: symbolToTryToImport
+            };
+        }
+        return Promise.resolve(this.runBuiltinSymbolSearch(
+            symbolToTryToImport).then((allFoundSymbolInfos): PromiseLike<ImportDetails> | ImportDetails => {
+                let symbolInfos = this.filterSymbolInformationToRelevant(allFoundSymbolInfos, symbolToTryToImport);
+                if (symbolInfos.length === 0) {
+                    return null;
                 }
+                if (symbolInfos.length > 1) {
+                    return this.pickSymbolToUseInImport(symbolInfos, symbolToTryToImport);
+                }
+                // or there is only one choice to try
+                return { symbolToImport: symbolToTryToImport, path: symbolInfos[0].location.uri.fsPath, symbolInfo: symbolInfos[0] };
             })).catch(e => {
                 vscode.window.showErrorMessage(`Encountered an error, sorry. Message: ${e.message}.`);
             });
-    });
+    }
 
-    context.subscriptions.push(disposable);
+    activate(context: vscode.ExtensionContext) {
+        let symbolToImport = this.guessSymbolToImport();
+        return Promise.resolve(this.searchForSymbolInWorkspace(symbolToImport)).then(importDetails => {
+            if (!importDetails) {
+                vscode.window.showInformationMessage(`Unable to locate symbol ${symbolToImport}`);
+            } else {
+                return this.addToImports(importDetails);
+            }
+        });
+    }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('activated');
+    context.subscriptions.push(
+        vscode.commands.registerCommand(tsImportAssistanceCommandName, () => {
+            new TSImportAssistance().activate(context);
+        }));
 }
